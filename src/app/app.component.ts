41d8e4d0b0d8d457fb2369bb95cb248f8cd5ca6f
abhijit_appcart <abhijit@appcartsystems.com>
2023-01-23 19:27:52 +0530

    Scss changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  rootElement: any;
+  ngOnInit() {
+    this.rootElement = document.documentElement;
+    // this.rootElement.setAttribute('data-theme', 'light');
+    this.rootElement.setAttribute('data-bs-theme', 'light');
+  }
+
+  switchTheme(ev: any) {
+    this.rootElement = document.documentElement;
+    // let checkExist = this.rootElement.getAttribute('data-theme');
+    let checkExist = this.rootElement.getAttribute('data-bs-theme');
+    if (checkExist == 'light') {
+      // this.rootElement.setAttribute('data-theme', 'dark');
+      this.rootElement.setAttribute('data-bs-theme', 'dark');
+    } else {
+      // this.rootElement.setAttribute('data-theme', 'light');
+      this.rootElement.setAttribute('data-bs-theme', 'light');
+    }
+  }
+}
